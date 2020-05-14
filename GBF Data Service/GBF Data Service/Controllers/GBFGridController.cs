1729912ca8c03278e2e150bae49bc05c6d2d902a
biondiylun <biondilun@hotmail.com>
2020-05-14 14:48:15 -0700

    Adding a data API to pull weapon data out of DB

---
+++ GBF Data Service/GBF Data Service/Controllers/GBFGridController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GBF_Data_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GBFGridController : ControllerBase
+    {
+
+        private readonly ILogger<GBFGridController> _logger;
+
+        public GBFGridController(ILogger<GBFGridController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public GBFWeapon Post([FromBody]int weaponId)
+        {
+            //fetch data from MongoDB based on weaponId
+            return new GBFWeapon();
+        }
+    }
+}
